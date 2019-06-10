5dc20b8809c355bbcf08974984f57a5d193dfc35
InYourXerXez <51115343+IyXerXez72@users.noreply.github.com>
2019-06-11 06:27:21 +0700

    Add files via upload

---
+++ baner.php
@@ -0,0 +1,147 @@
+<?php
+if (strtolower(substr(PHP_OS, 0, 3)) == "win") {
+    $bersih = "cls";
+} else {
+    $bersih = "clear";
+}
+
+date_default_timezone_set('Asia/Jakarta');
+$date = date('d-M-Y H:i:s');
+$green = "\e[92m";
+$red = "\e[91m";
+$yellow = "\e[93m";
+$blue = "\e[36m";
+$cyan = "\e[0;36m";
+$purple = "\e[0;35m";
+$brown = "\e[0;33m";
+$lightgray = "\e[0;37m";
+$nick = "XerXez7 Gans";
+$darkgray = "\e[1;30m";
+$lightblue = "\e[1;34m";
+$lightgreen = "\e[1;32m";
+$lightcyan = "\e[1;36m";
+$lightred = "\e[1;31m";
+$lightpurple = "\e[1;35m";
+pilih:
+    system($bersih);
+echo "$lightpurple
+SPESIAL THANK TO
+$red -$lightgray IyTn.Gus
+$red -$lightgray IyTakuRr
+$red -$lightgray Oficial MEMBER 2E4H&GBLG CREW$red
+";
+	echo ".";
+        sleep(1);
+        echo ".";
+        sleep(1);
+        echo ".";
+        sleep(1);
+        echo ".";
+        sleep(1);
+        echo ".";
+        sleep(1);
+        echo ".";
+        sleep(1);
+        echo ".";
+	echo ".";
+        sleep(1);
+        echo ".";
+        sleep(1);
+        echo ".";
+        sleep(1);
+        echo ".";
+        sleep(1);
+        echo ".";
+        sleep(1);
+        echo ".";
+        sleep(1);
+        echo ".";
+	echo ".";
+        sleep(1);
+        echo ".";
+        sleep(1);
+        echo ".";
+        sleep(1);
+        echo ".";
+        sleep(1);
+        echo ".";
+        sleep(1);
+        echo ".";
+        sleep(1);
+        echo ".";
+	echo "$darkgray";
+	echo ".";
+        sleep(1);
+        echo ".";
+        sleep(1);
+        echo ".";
+        sleep(1);
+        echo ".";
+        sleep(1);
+        echo ".";
+        sleep(1);
+        echo ".";
+        sleep(1);
+        echo ".";
+        echo ".";
+        sleep(1);
+        echo ".";
+        sleep(1);
+        echo ".";
+        sleep(1);
+        echo ".";
+        sleep(1);
+        echo ".";
+        sleep(1);
+        echo ".";
+        sleep(1);
+        echo ".";
+        echo ".";
+        sleep(1);
+        echo ".";
+        sleep(1);
+        echo ".";
+        sleep(1);
+        echo ".";
+        sleep(1);
+        echo ".";
+        sleep(1);
+        echo ".";
+        sleep(1);
+echo "$blue
+   / / /        /     /     /    /    /     /
+/   / /   / /     /  /     /        /   /  /
+$blue    ______________________________________
+$blue   /            /   /   /                /\
+$blue  /      /   /      /   /   /      /    /  \
+$blue /        /    /     /      /  /   /   /    \
+$blue [|]__________________________________[|]____|
+   |$brown  AU/THOR = InYourXerXez          $blue |    |
+   |$brown  Extensi= php+Bash               $blue |    |
+   |$brown  Title  = Uploader Scripy        $blue |  --|
+   |$brown  ThankTo= 2E4H-GBLG-BOGOR BLACK  $blue |  | |
+   |$brown           BUFT -OFICIAL NGEOD 😂 $blue |_____
+$blue [|]__________________________________[|]
+	ORAMG PALING BURIK = IyXerXez7
+KATNA SAYA TAU GANS ITU GA DI BAWA MATI
+-Z3X
+";
+echo "$lightpurple MASUK KRDALAM PROGRAM HEHE $$lightgreen";
+        echo ".";
+        sleep(1);
+        echo ".";
+        sleep(1);
+        echo ".";
+        sleep(1);
+        echo ".";
+        sleep(1);
+        echo ".";
+        sleep(1);
+        echo ".";
+        sleep(1);
+        echo ".";
+echo "
+";
+echo "$brown
+HAY GAN KAMU LOGIN PADA TANGGAL & JAM $date
+";
